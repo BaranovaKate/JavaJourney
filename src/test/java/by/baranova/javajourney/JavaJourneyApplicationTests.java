27af5bbf6a6bbc641348f174c3150dfd0c218515
BaranovaKate <katte.bar19@gmail.com>
2024-02-25 18:10:19 +0300

    add test

--- src/test/java/by/baranova/javajourney/JavaJourneyApplicationTests.java
+++ src/test/java/by/baranova/javajourney/JavaJourneyApplicationTests.java
@@ -2,12 +2,18 @@ package by.baranova.javajourney;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class JavaJourneyApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        assertNotNull(context);
     }
 
 }
